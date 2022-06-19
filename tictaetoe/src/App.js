22aea92ccb8cfdd3ba7480239b32a07d7401e290
ElaineHe <ElaineHe1233@outlook.co.nz>
2022-06-19 17:37:33 +1200

    tictaetoe
    tictaetoe game from react tutorial documents

---
+++ tictaetoe/src/App.js
@@ -0,0 +1,12 @@
+import Game from "./components/Game";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TicTaeToe</h1>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
